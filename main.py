e958093c67bb74c35b01220636b6676f6e7e6d30
kevinschwenger <91298935+kschwenger@users.noreply.github.com>
2021-09-27 19:50:30 +0000

    added sawtooth

--- main.py
+++ main.py
@@ -5,6 +5,35 @@
 – LED #3 continually blinks on/off at 1 Hz, independent other actions.
 – Use exception handling to exit elegantly on ctrl-C by cleaning up the GPIO ports."""
 
+# for triangles (LEDs 1 and 2)
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setmode(GPIO.BCM)
+p = 4
+GPIO.setup(p, GPIO.OUT)
+
+pwm = GPIO.PWM(p, 100) # create PWM object @ 100 Hz
+try:
+  pwm.start(0) # initiate PWM at 0% duty cycle
+  while 1:
+    for dc in range(101): # loop duty cycle from 0 to 100
+      pwm.ChangeDutyCycle(dc) # set duty cycle
+      sleep(0.01) # sleep 10 ms
+except KeyboardInterrupt: # stop gracefully on ctrl-C
+print('\nExiting’)
+
+pwm.stop()
+GPIO.cleanup()
+
+
+
+
+
+
+
+
+
 # for LED 3
 import RPi.GPIO as GPIO
 from time import sleep
