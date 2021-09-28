ccfd11366e5d6bc6e1a3fc2f1e41b49b85633fa9
kevinschwenger <91298935+kschwenger@users.noreply.github.com>
2021-09-28 23:25:37 +0000

    final version

--- main.py
+++ main.py
@@ -1,88 +1,55 @@
-"""Using a single threaded callback function, write python code such that:
-– When switch #1 is pressed, LED #1 produces a single cycle of a 1 Hz triangle waveform
-(smooth transition from low à high à low).
-– When switch #2 is pressed, LED #2 produces a single cycle of a 1 Hz triangle waveform.
-– LED #3 continually blinks on/off at 1 Hz, independent other actions.
-– Use exception handling to exit elegantly on ctrl-C by cleaning up the GPIO ports."""
+import RPi.GPIO as gpio # Import Raspberry Pi GPIO library and sleep function
+from time import sleep
 
-
-# for callback 
-import RPi.GPIO as gpio
-# Define input/output port numbers:
+# declare variables
 in1, in2 = 20, 21
-led1, led2, led3 = 4, 17, 27
+led1, led2, led3 = 4, 27, 13
 
-gpio.setmode(gpio.BCM)
-gpio.setup(in1, gpio.IN, pull_up_down=gpio.PUD_DOWN)
-gpio.setup(in2, gpio.IN, pull_up_down=gpio.PUD_DOWN)
-gpio.setup(led1, gpio.OUT)
+gpio.setmode(gpio.BCM) 
+gpio.setup(in1, gpio.IN, pull_up_down=gpio.PUD_DOWN) # Set pin 20 to be an input pin and set initial value to be pulled low (off)
+gpio.setup(in2, gpio.IN, pull_up_down=gpio.PUD_DOWN) # Set pin 21 to be an input pin and set initial value to be pulled low (off)
+gpio.setup(led1, gpio.OUT)      # set led pins as output pins
 gpio.setup(led2, gpio.OUT)
 gpio.setup(led3, gpio.OUT)
-# Define a threaded callback function:
-def myCallback1(channel):
-  gpio.output(led1, 1)
-def myCallback2(channel):
-  gpio.output(led2, 1)
 
-# Execute myCallback() if port 1 goes HIGH:
-gpio.add_event_detect(in1, gpio.RISING, callback=myCallback1, bouncetime=100)
-gpio.add_event_detect(in2, gpio.RISING, callback=myCallback2, bouncetime=100)
+pwm1 = gpio.PWM(led1, 100) # create PWM object @ 1 Hz for both leds
+pwm2 = gpio.PWM(led2, 100) 
 
-gpio.cleanup()
+def button_callback(channel): # callback function for triangle waveforms when button is pressed
 
-
-
-
-
-"""
-# for triangles (LEDs 1 and 2)
-import RPi.GPIO as GPIO
-from time import sleep
-
-GPIO.setmode(GPIO.BCM)
-p = 4
-GPIO.setup(p, GPIO.OUT)
-
-pwm = GPIO.PWM(p, 100) # create PWM object @ 1 Hz
-try:
-  pwm.start(0) # initiate PWM at 0% duty cycle
-  while 1:
+  if channel == in1:  # if button one is pressed
+    pwm1.start(0) # initiate PWM at 0% duty cycle
     for dc in range(101): # loop duty cycle from 0 to 100
-      pwm.ChangeDutyCycle(dc) # set duty cycle
-      sleep(0.01) # sleep 10 ms
-except KeyboardInterrupt: # stop gracefully on ctrl-C
-  print('\nExiting')
+      pwm1.ChangeDutyCycle(dc) # set duty cycle
+      sleep(0.005) # sleep 5 ms (.005*100 = .5)
+    pwm1.start(100) # initiate pwm at 100% duty cycle
+    for dc in range(100,-1,-1): # loop duty cycle from 100 to 0
+      pwm1.ChangeDutyCycle(dc)
+      sleep(0.005)
+  
+  if channel == in2:  # if button 2 is pressed
+      pwm2.start(0) 
+      for dc in range(101): 
+        pwm2.ChangeDutyCycle(dc) 
+        sleep(0.005) 
+      pwm2.start(100)
+      for dc in range(100,-1,-1): 
+        pwm2.ChangeDutyCycle(dc)
+        sleep(0.005)
 
-pwm.stop()
-GPIO.cleanup()
-"""
+gpio.add_event_detect(in1,gpio.RISING,callback=button_callback) # Setup event on pin 20 for RISING detection
+gpio.add_event_detect(in2,gpio.RISING,callback=button_callback) # Setup event on pin 21 for RISING detection
 
-
-
-
-
-
-
-
-# for LED 3
-"""
-import RPi.GPIO as GPIO
-from time import sleep
-
-GPIO.setmode(GPIO.BCM)
-p = 4
-GPIO.setup(p, GPIO.OUT)
-
+# continually blink led 3 at 1 hz unless keyboard interrupt
 try:
   while True:
-    GPIO.output(p, 0)
+    gpio.output(led3, 0)
     sleep(0.5)
-    GPIO.output(p, 1)
+    gpio.output(led3, 1)
     sleep(0.5)
 except KeyboardInterrupt: # if user hits ctrl-C
   print('\nExiting')
 except Exception as e: # catch all other errors
   print('\ne')
 
-GPIO.cleanup() # clean up GPIO ports
-"""
+gpio.cleanup() # Clean up
